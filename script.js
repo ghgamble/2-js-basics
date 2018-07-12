4b82955d4b05e5494d849651cf4e263f9a71a889
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-12 11:25:40 -0600

    Basic Operators

--- script.js
+++ script.js
@@ -26,24 +26,58 @@
 /* ****************************
  Variable Mutation and Type Coercion
 ******************************* */
-var firstName = 'John';
-var age = 28;
+// var firstName = 'John';
+// var age = 28;
 
 // Type Coercion
-console.log(firstName + ' ' + 28);
-
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
-
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// console.log(firstName + ' ' + 28);
+//
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+//
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 // Variable Mutation
 // Changing value of a variable after variable is already declared with a value above in the code
-age = 'twenty eight';
-job = 'driver';
+// age = 'twenty eight';
+// job = 'driver';
+//
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+//
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var lastName = prompt('What is his last name?');
-console.log(firstName + ' ' + lastName);
+/* ****************************
+ Basic Operators
+******************************* */
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
