f753d2e7c66f864a207f4b3b4c07ba562fd99f7c
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-12 10:39:03 -0600

    Variables and Data Types

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!!!");
+// console.log("Hello World!!!");
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'teacher';
+console.log(job);
+
+// All variable names must start with a $, _ or letter and can't be JS names
+// var 3years = 3;
