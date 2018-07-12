0e2708b4d8ce85ef2a2de2f886e03feca44bd9e2
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-12 11:08:57 -0600

    Variable Mutation and Type Coercion

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 // console.log("Hello World!!!");
 
-var firstName = 'John';
-console.log(firstName);
+/* ****************************
+ Variables and Data Types
+******************************* */
 
-var lastName = 'Smith';
-var age = 28;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job);
-job = 'teacher';
-console.log(job);
+// var firstName = 'John';
+// console.log(firstName);
+//
+// var lastName = 'Smith';
+// var age = 28;
+//
+// var fullAge = true;
+// console.log(fullAge);
+//
+// var job;
+// console.log(job);
+// job = 'teacher';
+// console.log(job);
 
 // All variable names must start with a $, _ or letter and can't be JS names
+// Variable naming rules
 // var 3years = 3;
+
+
+/* ****************************
+ Variable Mutation and Type Coercion
+******************************* */
+var firstName = 'John';
+var age = 28;
+
+// Type Coercion
+console.log(firstName + ' ' + 28);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable Mutation
+// Changing value of a variable after variable is already declared with a value above in the code
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
