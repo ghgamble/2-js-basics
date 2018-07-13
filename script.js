4dd3ca24f937e4122b839869a9629faa348de2a4
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-13 10:04:07 -0600

    Operator Precedence

--- script.js
+++ script.js
@@ -52,32 +52,75 @@
 /* ****************************
  Basic Operators
 ******************************* */
-var year, yearJohn, yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
 
 // Math Operators
-yearJohn = now - ageJohn;
-yearMark = now - ageMark;
-
-console.log(yearJohn);
-console.log(yearMark);
-
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+//
+// console.log(yearJohn);
+// console.log(yearMark);
+//
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
 
 // Logical Operators
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
-
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
+//
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
 
 // typeof Operator
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof 'Mark is older than John');
-var x;
-console.log(typeof x);
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than John');
+// var x;
+// console.log(typeof x);
+
+
+/* ****************************
+Operator Precedence
+******************************* */
+
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
+
+// Multiple Operators
+// var isFullAge = now - yearJohn >= fullAge;
+// True
+// console.log(isFullAge);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence TABLE
+
+// Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = ageJohn + ageMark / 2;         / takes precendence over +, so the calculation won't be read in the correct order
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
+
+// Multiple Assignments
+var x, y;
+console.log(x, y);
+x = y = (3 + 5) * 4 - 6;
+// 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More Operators
+// x = x * 2;
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+// All three are the same effect
+// x = x + 1;
+// x += 1;
+x++;
+console.log(x);
