d43417d0525e8d432e720a0f51a4c43ffddd2d27
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-16 10:22:44 -0600

    Truthy and Falsy Values and Equality Operators

--- script.js
+++ script.js
@@ -245,34 +245,58 @@ The Ternary Operator and Switch Statements
 // }
 
 //Switch Statement
-var firstName = 'John';
-var job = 'teacher';
-switch (job) {
-      case 'instructor':
-      case 'teacher':
-            console.log(firstName + ' teaches kids how to code.');
-            break;
-      case 'driver':
-            console.log(firstName + ' drives an uber in Boulder.');
-            break;
-      case 'designer':
-            console.log(firstName + ' designs beautiful websites.');
-            break;
-      default:
-            console.log(firstName + ' does something else.');
+// var firstName = 'John';
+// var job = 'teacher';
+// switch (job) {
+//       case 'instructor':
+//       case 'teacher':
+//             console.log(firstName + ' teaches kids how to code.');
+//             break;
+//       case 'driver':
+//             console.log(firstName + ' drives an uber in Boulder.');
+//             break;
+//       case 'designer':
+//             console.log(firstName + ' designs beautiful websites.');
+//             break;
+//       default:
+//             console.log(firstName + ' does something else.');
+// }
+//
+// var age = 12;
+// switch (true) {
+//       case age < 13:
+//             console.log(firstName + ' is a boy.');
+//             break;
+//       case age >= 13 && age < 20:
+//             console.log(firstName + ' is a teenager.');
+//             break;
+//       case age >= 20 && age < 30:
+//             console.log(firstName + ' is a young man.');
+//             break;
+//       default:
+//             console.log(firstName + ' is a man.');
+// }
+
+
+/* ****************************
+Truthy and Falsy Values and Equality Operators
+******************************* */
+
+// Falsy Values: undefined, null, 0, '', NaN
+// Truthy Values: NOT falsy Values
+
+var height;
+
+height = 23;
+
+if (height) {
+      console.log('Variable is defined');
+}
+else {
+      console.log('Variable has NOT been defined');
 }
 
-var age = 12;
-switch (true) {
-      case age < 13:
-            console.log(firstName + ' is a boy.');
-            break;
-      case age >= 13 && age < 20:
-            console.log(firstName + ' is a teenager.');
-            break;
-      case age >= 20 && age < 30:
-            console.log(firstName + ' is a young man.');
-            break;
-      default:
-            console.log(firstName + ' is a man.');
+// Equality Operators
+if (height == '23') {
+      console.log('The == operator does type coercion!');
 }
