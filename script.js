5bf5341ffbb55b165a592cd6dedc4afe4a3738f2
Grace Gamble <grace.h.gamble@gmail.com>
2018-07-26 14:08:36 -0600

    Coding Challenge 5: Instructor Soltution Part 2

--- script.js
+++ script.js
@@ -728,5 +728,53 @@ var john = {
       }
 }
 
+// Instructor Solution Part 2
+
+var mark = {
+      fullName: 'Mark Miller',
+      bills: [77, 375, 110, 45],
+      calcTips: function () {
+            this.tips = [];
+            this.finalValues = [];
+            for (var i = 0; i < this.bills.length; i++) {
+                  // Determine percentage based on tipping rules
+                  var percentage;
+                  var bill = this.bills[i];
+                  if (bill < 100) {
+                        percentage = 0.20;
+                  } else if (bill <= 100 && bill < 300) {
+                        percentage = 0.10;
+                  } else {
+                        percentage = 0.25;
+                  }
+                  // Add result to the corresponding arrays
+                  this.tips[i] = bill * percentage;
+                  this.finalValues[i] = bill + bill * percentage;
+            }
+      }
+}
+
+function calcAverage(tips) {
+      var sum = 0;
+      for (var i = 0; i < tips.length; i++) {
+            sum = sum + tips[i];
+      }
+      return sum / tips.length;
+}
+
+// Do the calculations for tips and final values
 john.calcTips();
-console.log(john);
+mark.calcTips();
+console.log(john, mark);
+// Calculate average
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+      console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (john.average < mark.average) {
+      console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+} else {
+      console.log('Mark and John pay the same average tip amount.');
+}
